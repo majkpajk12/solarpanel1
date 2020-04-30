dd3122d2493549ed30f9883adc098ac80dac3ea8
Mikael Simonsson <minemail7@gmail.com>
2020-04-30 04:40:32 +0100

    updating our styles to use media queries for mobile

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -10,7 +10,7 @@ import {
   BackgroundImage,
   NameContainer,
   PriceContainer
-} from './collection-styles.styles';
+} from './collection-item.styles';
 
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
@@ -37,3 +37,49 @@ export default connect(
   null,
   mapDispatchToProps
 )(CollectionItem);
+
+
+
+
+
+
+
+// import React from 'react';
+// import { connect } from 'react-redux';
+
+// import { addItem } from '../../redux/cart/cart.actions';
+
+// import {
+//   CollectionItemContainer,
+//   CollectionFooterContainer,
+//   AddButton,
+//   BackgroundImage,
+//   NameContainer,
+//   PriceContainer
+// } from './collection-styles.styles';
+
+// const CollectionItem = ({ item, addItem }) => {
+//   const { name, price, imageUrl } = item;
+
+//   return (
+//     <CollectionItemContainer>
+//       <BackgroundImage className='image' imageUrl={imageUrl} />
+//       <CollectionFooterContainer>
+//         <NameContainer>{name}</NameContainer>
+//         <PriceContainer>{price}</PriceContainer>
+//       </CollectionFooterContainer>
+//       <AddButton onClick={() => addItem(item)} inverted>
+//         Add to cart
+//       </AddButton>
+//     </CollectionItemContainer>
+//   );
+// };
+
+// const mapDispatchToProps = dispatch => ({
+//   addItem: item => dispatch(addItem(item))
+// });
+
+// export default connect(
+//   null,
+//   mapDispatchToProps
+// )(CollectionItem);
